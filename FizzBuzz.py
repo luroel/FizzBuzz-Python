430e19534b6c1f1882211fee879d121bc6834a7e
luroel <jl.rodrigueze@hotmail.com>
2024-06-04 13:57:15 -0600

    Imprime Fizz y Buzz si son multiplos de 3 y 5

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+#Logica de programación Python 
+# FizzBuzz algorithm 
+
+for x in range(1,101):
+    if x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Bazz")
+    else:
+        print(x)
+
