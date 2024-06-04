604ac0402b0c6d5383ad7194673d02dec68f95c4
luroel <jl.rodrigueze@hotmail.com>
2024-06-04 14:08:30 -0600

    Imprimiendo FizzBuzz si son multiplos por igual de 3 y 5

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,10 +2,11 @@
 # FizzBuzz algorithm 
 
 for x in range(1,101):
-    if x % 3 == 0:
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
         print("Fizz")
     elif x % 5 == 0:
-        print("Bazz")
+        print("Buzz")
     else:
         print(x)
-
